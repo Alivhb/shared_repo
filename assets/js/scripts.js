03545eeaf58fad98f40008c57867ad5d12970282
letqin <86627655+letqin@users.noreply.github.com>
2021-08-28 13:25:11 -0400

    pseudo code for javascript

---
+++ assets/js/scripts.js
@@ -0,0 +1,8 @@
+// user can submit a search query from the application
+
+
+// search can be generic or can include formats to filter results
+
+// results are displayed on a separate page
+
+// additional searches can be made from the results page
